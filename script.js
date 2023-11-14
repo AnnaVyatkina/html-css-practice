ee9ba5f6dcba89919563e96225dc5bf8c9c2859a
Anna Vyatkina <138256395+AnnaVyatkina@users.noreply.github.com>
2023-11-14 19:28:10 +0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-function animateEmoji() {
-    const emoji = document.querySelector('.trick');
-    emoji.style.fontSize = '30vw';
-    setTimeout(() => {
-        emoji.style.fontSize = '0';
-    }, 500);
-}
